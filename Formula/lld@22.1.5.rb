b6b81d75b7acdf8fcc8b0c5618a05a966567a061
hydai <z54981220@gmail.com>
2026-05-28 17:59:19 +0800

    Update static lld for LLVM 22.1.6 (#6)
    Signed-off-by: hydai <z54981220@gmail.com>

--- Formula/lld@22.1.5.rb
+++ Formula/lld@22.1.5.rb
@@ -1,8 +1,8 @@
-class Lld < Formula
+class LldAT2215 < Formula
   desc "LLVM Project Linker"
   homepage "https://lld.llvm.org/"
-  url "https://github.com/llvm/llvm-project/releases/download/llvmorg-22.1.6/llvm-project-22.1.6.src.tar.xz"
-  sha256 "6e0b376a1f6d9873e7dfb09ae6e04b9c7024400f01733fa4c29be69d5c138bc2"
+  url "https://github.com/llvm/llvm-project/releases/download/llvmorg-22.1.5/llvm-project-22.1.5.src.tar.xz"
+  sha256 "7972b87b705a003ce70ab55f9f0fb495d156887cba0eb296d284731139118e2c"
   # The LLVM Project is under the Apache License v2.0 with LLVM Exceptions
   license "Apache-2.0" => { with: "LLVM-exception" }
   compatibility_version 1
@@ -12,19 +12,11 @@ class Lld < Formula
     formula "llvm"
   end
 
-  bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "a31b068684bb8d00709d6ddb4599ad3c882951e8da8f6038ca38f2e3ed2c540f"
-    sha256 cellar: :any,                 arm64_sequoia: "4df2947859b69d09f812b8061a3f7c8064cec017960f2dc6e34dfe3322c3da43"
-    sha256 cellar: :any,                 arm64_sonoma:  "301c6e2979c6ebd84bb615951485f7c2f1d112e71d469ea50af6cc2539c134f4"
-    sha256 cellar: :any,                 sonoma:        "0ce0177373c987e05f9a103fcb71e60b4e915e93233a9ff852e9c26f3a6b9d85"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "eb54c8f962f3b542f31356de7f5dcb29d1a99399122b7ab52e87dc4907999b1b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4a8192739b1938b639a3b1e0f9c84ce9468e81e25dabbe8c0e894b8f3adb296a"
-  end
+  keg_only :versioned_formula
 
   depends_on "cmake" => :build
   depends_on "llvm"
   depends_on "zstd"
-
   on_linux do
     depends_on "zlib-ng-compat"
   end
